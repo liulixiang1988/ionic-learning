649d150e85fd2bb797e62c8c957a234a70e21c95
Liu Lixiang <liulixiang1988@gmail.com>
2015-06-07 16:14:32 +0800

    learn how tu use ionic tab

---
+++ my-tabs/www/js/app.js
@@ -0,0 +1,47 @@
+(function(){
+app = angular.module('starter', ['ionic'])
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider.state('home', {
+    url: '/home',
+    views:{
+      'tab-home':{
+        templateUrl: 'templates/home.html'
+      }
+    }
+  });
+  $stateProvider.state('details', {
+    url: '/details',
+    views:{
+      'tab-home':{
+        templateUrl: 'templates/details.html'
+      }
+    }
+  });
+
+  $stateProvider.state('settings', {
+    url: '/settings',
+    views:{
+      'tab-settings':{
+        templateUrl: 'templates/settings.html'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/home');
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+}());

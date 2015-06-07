7d6b456bc66fe63726fb2e306bb19fae8ba599cb
Liu Lixiang <liulixiang1988@gmail.com>
2015-06-07 17:19:27 +0800

    add sidemenu

---
+++ my-sidemenu/www/js/app.js
@@ -0,0 +1,34 @@
+(function(){
+app = angular.module('starter', ['ionic']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider.state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html'
+  });
+  $stateProvider.state('details', {
+    url: '/details',
+    templateUrl: 'templates/details.html'
+  });
+
+  $stateProvider.state('settings', {
+    url: '/settings',
+    templateUrl: 'templates/settings.html'
+  });
+
+  $urlRouterProvider.otherwise('/home');
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});  
+}())
